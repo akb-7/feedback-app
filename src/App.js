fbe0b7872813f1d75be710eac9171c57297b2c28
akb-7 <aakash.super77@gmail.com>
2022-02-09 17:23:47 +0530

    Added the jsx tut component

---
+++ src/App.js
@@ -0,0 +1,32 @@
+function App() {
+  const title = 'Blog Post'
+  const body = 'This is my blog post'
+  const comments = [
+	  {id:1,text:"comment 1"},
+	  {id:2,text:"comment 2"},
+	  {id:3,text:"comment 3"},
+	  {id:4,text:"comment 4"}
+  ]
+  const loading = false;
+  const showComments = true;
+
+  if(loading) return <h1>Loading....</h1>
+  return (
+    <div className="App">
+        <h1>{title}</h1>
+        <p>{body}</p>
+        {showComments &&
+        <div className="comments">
+        <h3>Comments ({comments.length})</h3>
+        <ul>
+          {comments.map((comment,index)=>(
+            <li key={index}>{comment.text}</li>
+          ))}
+        </ul>
+      </div>}
+        
+    </div>
+  );
+}
+
+export default App;
