4a934354561c51e6faed58e8b64deba9e3ae9ca0
akb-7 <aakash.super77@gmail.com>
2022-02-10 15:11:00 +0530

    Added button clickevents

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -3,13 +3,18 @@ import {useState} from 'react'
 function FeedbackItem() {
     const [rating,setRating] = useState(7) // have name and setName
     const [text,setText] = useState("This is an example of feedback item") // have name and setName
-
+    const handleClick = () =>{
+        setRating((prev)=>{
+            console.log(prev);
+            return prev+1
+        });
+    }
+    
   return (
     <div className="card">
         <div className="num-display">{rating}</div>
-        <div className="text-display">
-            {text}
-        </div>
+        <div className="text-display">{text}</div>
+        <button onClick={handleClick}>Click me </button>
     </div>
   )
 }
