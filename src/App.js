8b09202797b1be0fcf7a34f80d24f924ba135862
akb-7 <aakash.super77@gmail.com>
2022-02-10 15:28:45 +0530

    Added the global feedback list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Header from "./components/Header";
-import FeedbackItem from "./components/FeedbackItem";
+import { useState } from "react";
+import FeedbackData from "./data/FeedbackData";
+import FeedbackList from "./components/FeedbackList";
 function App() {
+  const [feedback,setFeedback] = useState(FeedbackData);
+  console.log(feedback);
   return (
     <>
     <Header text='Feedback UI'/>
     <div className="App">
-      <FeedbackItem/>
+      <FeedbackList feedback={feedback}/>
     </div>
     </>
   );
